ffb3731bd59a17630ca28f5e602e7e70a975df8d
KoenPothof <koenpothof@gmail.com>
2023-01-31 14:33:35 +0100

    Update Spirograph.java

--- Week1/005.Spirograph/src/Spirograph.java
+++ Week1/005.Spirograph/src/Spirograph.java
@@ -2,9 +2,14 @@ import java.awt.*;
 import java.awt.geom.*;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import static javax.swing.text.StyleConstants.getBackground;
+
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -16,7 +21,9 @@ public class Spirograph extends Application {
     private TextField v2;
     private TextField v3;
     private TextField v4;
-    
+    private Canvas canvas = new Canvas(1920, 1080);
+    boolean buttonState = false;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         VBox vBox = new VBox();
@@ -51,17 +58,34 @@ public class Spirograph extends Application {
 
         primaryStage.setScene(new Scene(vBox));
         primaryStage.setTitle("Spirograph");
+
         primaryStage.show();
     }
     
-    
     public void draw(FXGraphics2D graphics) {
-        //you can use Double.parseDouble(v1.getText()) to get a double value from the first textfield
-        //feel free to add more textfields or other controls if needed, but beware that swing components might clash in naming
+        double resolution = 0.0001;
+        double scale = 0.7;
+
+        float x =0;
+        float y =0;
+        float x2 =0;
+        float y2 =0;
+        float set1 =(float)Double.parseDouble((v1.getText()));
+        float set2 =(float)Double.parseDouble((v2.getText()));
+        float set3 =(float)Double.parseDouble((v3.getText()));
+        float set4 =(float)Double.parseDouble((v4.getText()));
+
+        for (double i = 0; i < 2* Math.PI ; i+= resolution) {
+            graphics.setColor(Color.getHSBColor((float) (i/Math.PI), 1, 1));
+
+            graphics.draw(new Line2D.Double(x*scale, y*scale, x2*scale, y2*scale));
+            x2 = set1*(float) Math.cos(set2 * i) + set3*(float) Math.cos(set4*i);
+            y2 = set1*(float) Math.sin(set2 * i) + set3*(float) Math.sin(set4*i);
+            x = x2;
+            y = y2;
+        }
     }
-    
-    
-    
+
     public static void main(String[] args) {
         launch(Spirograph.class);
     }
