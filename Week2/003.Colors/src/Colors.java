c213602525943052042ef5b3f423fce242b8f889
KoenPothof <koenpothof@gmail.com>
2023-02-07 11:44:10 +0100

    Color update

--- Week2/003.Colors/src/Colors.java
+++ Week2/003.Colors/src/Colors.java
@@ -33,6 +33,45 @@ public class Colors extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+
+        for (int i = 0; i < 13; i++) {
+            Rectangle2D rec = new Rectangle2D.Double(i*100, 10, 50, 50);
+            graphics.setColor(Color.BLACK);
+            graphics.draw(rec);
+            if(i==0) {
+                graphics.setColor(Color.BLACK);
+            } else if (i==1) {
+                graphics.setColor(Color.BLUE);
+            } else if (i==2) {
+                graphics.setColor(Color.CYAN);
+            } else if (i==3) {
+                graphics.setColor(Color.DARK_GRAY);
+            }else if (i==4){
+                graphics.setColor(Color.GRAY);
+            } else if (i==5) {
+                graphics.setColor(Color.GREEN);
+            } else if (i==6) {
+                graphics.setColor(Color.LIGHT_GRAY);
+            } else if (i==7) {
+                graphics.setColor(Color.MAGENTA);
+            } else if (i==8) {
+                graphics.setColor(Color.ORANGE);
+            } else if (i==9) {
+                graphics.setColor(Color.pink);
+            } else if (i==10) {
+                graphics.setColor(Color.red);
+            } else if (i==11) {
+                graphics.setColor(Color.white);
+            } else if (i==12) {
+                graphics.setColor(Color.yellow);
+            }
+
+            graphics.fill(rec);
+        }
+
+
+
     }
 
 
