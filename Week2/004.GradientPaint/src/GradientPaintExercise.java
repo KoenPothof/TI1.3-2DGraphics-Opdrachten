b6280d47d323af365d3b0b8418e5630754783bb8
KoenPothof <koenpothof@gmail.com>
2023-02-07 13:14:35 +0100

    GradientPaint update

--- Week2/004.GradientPaint/src/GradientPaintExercise.java
+++ Week2/004.GradientPaint/src/GradientPaintExercise.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 
@@ -14,6 +15,7 @@ import org.jfree.fx.ResizableCanvas;
 
 public class GradientPaintExercise extends Application {
     private ResizableCanvas canvas;
+    private Point2D position;
 
     @Override
     public void start(Stage primaryStage) throws Exception
@@ -21,6 +23,13 @@ public class GradientPaintExercise extends Application {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
+        position = new Point2D.Double(canvas.getWidth()/2,canvas.getHeight()/2);
+        canvas.setOnMouseDragged(e ->
+        {
+            position = new Point2D.Float((int) e.getX(), (int) e.getY());
+            draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+        });
+
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("GradientPaint");
         primaryStage.show();
@@ -31,8 +40,15 @@ public class GradientPaintExercise extends Application {
     public void draw(FXGraphics2D graphics)
     {
         graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
+        float[] dist = {0.1f, 0.2f, 1.0f};
+        Color[] colors = {Color.RED, Color.WHITE, Color.BLUE};
+        float radius = 100;
+
+        RadialGradientPaint p = new RadialGradientPaint(position, radius, dist, colors);
+
+        graphics.setPaint(p);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
     }
 
 
